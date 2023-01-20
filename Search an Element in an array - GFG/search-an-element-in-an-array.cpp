13da7d479c9c900596cc99ded1a5d91733115077
Prashant Kumar <56153593+Prashant8722@users.noreply.github.com>
2023-01-20 23:37:47 +0530

    Added solution - LeetHub

---
+++ Search an Element in an array - GFG/search-an-element-in-an-array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int search(int arr[], int n, int X)
+    {   int count = -1;
+        for(int i=0; i < n ; i++)
+        {   
+                count++;
+            if(arr[i] == X)
+                return count;
+        }
+       
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        int sizeOfArray;
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        int x;
+        
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            cin>>arr[i];
+        }
+        cin>>x;
+        Solution ob;
+        cout<<ob.search(arr,sizeOfArray,x)<<endl; //Linear search
+    }
+
+    return 0;
+    
+}
+
+// } Driver Code Ends
