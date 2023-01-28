de16f08fb6f4577836ca3b120a2489a403dc6671
Prashant Kumar <56153593+Prashant8722@users.noreply.github.com>
2023-01-28 18:58:38 +0530

    Added solution - LeetHub

--- Lucky alive person in a circle - GFG/lucky-alive-person-in-a-circle.cpp
+++ Lucky alive person in a circle - GFG/lucky-alive-person-in-a-circle.cpp
@@ -5,14 +5,47 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
+    int josh(vector<int> &v , int k , int ind)
+    {
+        if(v.size() == 1)
+            {
+                return v[0];
+            }
+        ind = (ind + k) % v.size();
+        v.erase(v.begin() + ind);
+        //   for(int i = 0 ; i< v.size() ; i++)
+        // {
+        //     cout<<v[i]<<" ";
+        // }
+        // cout<<endl;
+        josh(v,k,ind);
+    }
+    
     int find(int N){
-         int temp = 1;
-         while(temp<=N)
-         {
-             temp *= 2 ;
-         }
-         temp =temp /2 ;
-         return 2*(N-temp) + 1;
+        /* method 1
+        //  int temp = 1;
+        //  while(temp<=N)
+        //  {
+        //      temp *= 2 ;
+        //  }
+        //  temp =temp /2 ;
+        //  return 2*(N-temp) + 1;
+        */
+        vector<int>v;
+        int k = 2;
+        k--;     //personNoKilled
+        int ind = 0 ;
+         for(int i = 0 ; i< N ; i++)
+        {
+            v.push_back(i+1);
+        }
+        // for(int i = 0 ; i< N ; i++)
+        // {
+        //     cout<<v[i]<<" ";
+        // }
+        //cout<<endl;
+        return josh(v,k,ind);
+       //return 0; 
     }
 };
 
