d95cb56298be89df386bc2d5851c8f77fb82bfe7
Prashant Kumar <56153593+Prashant8722@users.noreply.github.com>
2023-02-03 09:30:28 +0530

    Added solution - LeetHub

---
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+//Function to merge two sorted linked list.
+Node* sortedMerge(Node* n, Node* m)  
+{  
+    // code here
+    Node *temp =NULL;
+    Node *track  = NULL;
+    Node *res =NULL;
+    while( n != NULL && m != NULL )
+    {
+        if(m->data < n->data ){
+            temp = m;
+            m = m->next;
+            temp->next = NULL;
+        }
+        else{
+            temp = n;
+            n = n->next;
+            temp->next = NULL; 
+        }
+        
+        if(!res){
+            res = temp;
+            track = res;
+            temp  = NULL;
+        }
+        else{
+        track->next = temp;
+        track = track -> next;
+        }
+        if(n == NULL){
+            track->next = m;
+        }
+        if(m==NULL){
+                track -> next = n; 
+            }
+    }
+    
+    return res;
+}
