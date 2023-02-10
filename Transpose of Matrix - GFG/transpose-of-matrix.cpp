ca8caf3f382ea1d07c8048c457191880cfd5a194
Prashant Kumar <56153593+Prashant8722@users.noreply.github.com>
2023-02-10 20:37:50 +0530

    Added solution - LeetHub

--- Transpose of Matrix - GFG/transpose-of-matrix.cpp
+++ Transpose of Matrix - GFG/transpose-of-matrix.cpp
@@ -9,6 +9,7 @@ class Solution
     //Function to find transpose of a matrix.
     void transpose(vector<vector<int> >& m, int n)
     { 
+        /* method 1 O(n^2) time and space now reduce space complexity
         int temp[n][n];
         for(int i = 0 ; i < n ;i++)
             for(int j = 0 ; j < n ;j++)
@@ -20,6 +21,14 @@ class Solution
              {
                  m[i][j] = temp[i][j]; 
              }
+             */
+             //method 2
+             for(int i = 0 ; i < n ;i++)
+            for(int j = i+1 ; j < n ;j++)
+             {
+                 swap(m[i][j] , m[j][i]); 
+             }
+             
     }
 };
 
