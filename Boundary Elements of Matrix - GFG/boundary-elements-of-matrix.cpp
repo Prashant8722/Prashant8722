9c2b16949b23ae403980b353ff765bf39d03c716
Prashant Kumar <56153593+Prashant8722@users.noreply.github.com>
2023-02-10 20:00:09 +0530

    Added solution - LeetHub

---
+++ Boundary Elements of Matrix - GFG/boundary-elements-of-matrix.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    vector<int> BoundaryElements(vector<vector<int>>&mat){
+        vector<int>v;
+        int n = mat.size(); //  row && column;
+            if(n==1)
+            {
+                v.push_back(mat[0][0]);
+            }
+            else if(n == 2)
+            {
+                for(int i = 0;i<n ;i++)
+                {
+                    for(int j= 0 ; j<n ;j++)
+                        v.push_back(mat[i][j]);
+                }
+            }
+            else {
+                for(int j = 0 ; j < n ;j++)
+                {   
+                    v.push_back(mat[0][j]);
+                }
+                for(int i = 1;i<n-1; i++)
+                {
+                    v.push_back(mat[i][0]);
+                    v.push_back(mat[i][n-1]);
+                }
+                for(int j = 0 ; j< n ; j++)
+                {
+                    v.push_back(mat[n-1][j]);
+                }
+            }
+            return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>matrix(n, vector<int>(n, 0));
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < n; j++)
+				cin >> matrix[i][j];
+		Solution obj;
+		vector<int>ans = obj.BoundaryElements(matrix);
+		for(auto i: ans)
+			cout << i << " ";
+		cout << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
